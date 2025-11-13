7cd1870935e94827030119aeca5345d8b49ab903
agipssi <agippsi@proton.me>
2025-11-13 15:21:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<?php
-phpinfo();
 
-echo "<hr><h2>Test connexion MySQL</h2>";
-
-$host = 'mysql';
-$db = 'ma_base';
-$user = 'user';
-$pass = 'user123';
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    echo "<p style='color: green;'>✅ Connexion MySQL réussie!</p>";
-} catch(PDOException $e) {
-    echo "<p style='color: red;'>❌ Erreur MySQL: " . $e->getMessage() . "</p>";
-}
-?>
